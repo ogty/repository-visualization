97b84c146d158cb7d6a93fd0c3684b4004677630
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-02-17 04:46:14 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -20,36 +20,88 @@ class LambdaException(Exception):
             "errorMessage": self.error_message
         }
 
+
 class GenerateTree:
     
     def __init__(self, username: str, repository_name: str, default_branch: str) -> None:
         self.username = username
         self.repository_name = repository_name
         self.default_branch = default_branch
+        self.repository_path = f"/{self.username}/{self.repository_name}"
+        self.root = {
+            repository_name: []
+        }
 
-    def tmp(self) -> List[str]:
-        pass
+        files_or_folders = self.get_first_level_files_or_folders()
+        for file_or_folder in files_or_folders:
+            self.below_second_level(file_or_folder)
 
-    def scraping(self) -> List[str]:
+    def below_second_level(self, path: str) -> None:
+        print(path)
+        result = {
+            "name": path.split("/")[-1],
+            "files": []
+        } 
+
+        blob_or_tree_path = path \
+            .replace(f"{self.repository_path}/tree", self.repository_path) \
+            .replace(f"{self.repository_path}/blob", self.repository_path) \
+            .split("/")
+
+        blob_path = blob_or_tree_path.copy()
+        tree_path = blob_or_tree_path.copy()
+        
+        blob_path.insert(3, "blob")
+        tree_path.insert(3, "tree")
+        
+        blob_path = "/".join(blob_path)
+        tree_path = "/".join(tree_path)
+
+        html = requests.get(urljoin("https://github.com/", path))
+        soup = BeautifulSoup(html.content, "html.parser")
+        paths_for_repository = [i.get("href") for i in soup.find_all("a")]
+
+        process = [f"url.startswith('{i}')" for i in [blob_path, tree_path]]
+        process_word = " or ".join(process)
+        next_paths = []
+        for url in paths_for_repository:
+            if eval(process_word):
+                if not url.startswith(path):
+                    result["files"].append({"name": url.split("/")[-1]})
+                    next_paths.append(url)
+
+        if not result["files"]:
+            result.pop("files")
+
+        parent = path.split("/")[-2] if path.split("/")[-2] != self.default_branch else self.repository_name
+        if parent == self.repository_name:
+            self.root[self.repository_name].append(result)
+
+        # TODO: else?
+
+        for file in next_paths:
+            self.below_second_level(file)
+    
+    def get_first_level_files_or_folders(self) -> List[str]:
         repository_path = f"/{self.username}/{self.repository_name}"
         url = urljoin("https://github.com/", repository_path)
         html = requests.get(url)
         soup = BeautifulSoup(html.content, "html.parser")
         paths_for_repository = [i.get("href") for i in soup.find_all("a")]
-        
-        blob_path = f"{repository_path}/blob/{self.default_branch}/"
-        tree_path = f"{repository_path}/tree/{self.default_branch}/"
 
-        result = []
+        process = [f"url.startswith('{repository_path}/{i}/{self.default_branch}')" for i in ["blob", "tree"]]
+        process_word = " or ".join(process)
+
+        result = set()
         for url in paths_for_repository: 
-            if url.startswith(blob_path) or url.startswith(tree_path):
+            if eval(process_word):
                 if url.count("/") == 5:
-                    result.append(url)
+                    result.add(url)
 
         return result
-    
-    def __call__(self) -> Dict[str]:
-        pass
+
+    def __call__(self) -> Dict[str, list]:
+        return self.root
 
 
 def lambda_handler(event, content):
@@ -57,7 +109,7 @@ def lambda_handler(event, content):
     username = data["userName"]
     repository_name = data["repositoryName"]
 
-    response = requests.get('https://api.github.com/users/ogty/requirements.txt-generator')
+    response = requests.get("https://api.github.com/users/ogty/requirements.txt-generator")
     try:
         response.raise_for_status()
     except HTTPError as e:
@@ -67,7 +119,7 @@ def lambda_handler(event, content):
     else:
         data = response.json()
         default_branch = data[0]["default_branch"]
-        GenerateTree(username, repository_name, default_branch)
+        body_cotent = GenerateTree(username, repository_name, default_branch)
         return {
             "statusCode": 200,
             "headers": {
@@ -75,8 +127,9 @@ def lambda_handler(event, content):
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS,POST"
             },
-            "body": json.dump()
+            "body": json.dumps(body_cotent)
         }
 
 if __name__ == "__main__":
-    print(GenerateTree("ogty", "requirements.txt-generator", "master").scraping())
+    result = GenerateTree("ogty", "uiux", "master")
+    print(result())
