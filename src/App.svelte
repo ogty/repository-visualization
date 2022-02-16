75b5accce1cad7a6d7137ee3116de9cea11b21a5
ogty <mofuiti1803@gmail.com>
2022-02-17 00:34:47 +0900

    feat: display tree structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script>
-	export let name;
+	import Folder from './components/Folder.svelte';
+
+	let root = [
+		{
+			name: 'Important work stuff',
+			files: [
+				{ name: 'quarterly-results.xlsx' }
+			]
+		},
+		{
+			name: 'Animal GIFs',
+			files: [
+				{
+					name: 'Dogs',
+					files: [
+						{ name: 'treadmill.gif' },
+						{ name: 'rope-jumping.gif' }
+					]
+				},
+				{
+					name: 'Goats',
+					files: [
+						{ name: 'parkour.gif' },
+						{ name: 'rampage.gif' }
+					]
+				},
+				{ name: 'cat-roomba.gif' },
+				{ name: 'duck-shuffle.gif' },
+				{ name: 'monkey-on-a-pig.gif' }
+			]
+		},
+		{ name: 'TODO.md' }
+	];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Folder name="Home" files={root} expanded/>
---
+++ src/components/File.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+	$: type = name.slice(name.lastIndexOf('.') + 1);
+</script>
+
+<span style="background-image: url(/icons/{type}.svg)">{name}</span>
+
+<style>
+	span {
+		padding: 0 0 0 1.5em;
+		background: 0 0.1em no-repeat;
+	}
+</style>
